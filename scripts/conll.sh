9cce2b3b3a7dc7d4b8f081853e4a750d80b80220
Iker <igarciaf896@gmail.com>
2021-02-12 12:51:22 +0100

    Experiments params

--- scripts/conll.sh
+++ scripts/conll.sh
@@ -6,9 +6,11 @@ MRC_DROPOUT=0.3
 LR=3e-5
 SPAN_WEIGHT=0.1
 WARMUP=0
-MAXLEN=128
+MAXLEN=150
 MAXNORM=1.0
-
+BATCH_SIZE=16
+GRADIENT_ACCUMULATION_STEPS=2
+WORKERS=8
 OUTPUT_DIR="/ikerlariak/igarcia945/NerQA/bert-base-uncased"
 mkdir -p $OUTPUT_DIR
 
@@ -17,14 +19,13 @@ python3 trainer.py \
 --data_dir $DATA_DIR \
 --bert_config_dir $BERT_DIR \
 --max_length $MAXLEN \
---batch_size 4 \
---gpus="4" \
+--batch_size $BATCH_SIZE \
+--gpus="4," \
 --precision=16 \
 --progress_bar_refresh_rate 1 \
 --lr $LR \
---distributed_backend=ddp \
 --val_check_interval 0.5 \
---accumulate_grad_batches 2 \
+--accumulate_grad_batches $GRADIENT_ACCUMULATION_STEPS \
 --default_root_dir $OUTPUT_DIR \
 --mrc_dropout $MRC_DROPOUT \
 --bert_dropout $BERT_DROPOUT \
@@ -33,4 +34,6 @@ python3 trainer.py \
 --weight_span $SPAN_WEIGHT \
 --warmup_steps $WARMUP \
 --max_length $MAXLEN \
---gradient_clip_val $MAXNORM
+--gradient_clip_val $MAXNORM \
+--flat \
+--workers $WORKERS 
--- scripts/conll_paste.sh
+++ scripts/conll_paste.sh
@@ -1,15 +1,15 @@
+mkdir -p "/ikerlariak/igarcia945/NerQA/conll03/"
 python3 trainer.py \
 --data_dir "/ikerlariak/igarcia945/NerQA/conll03/" \
 --bert_config_dir "bert-base-uncased" \
 --max_length 128 \
---batch_size 1 \
+--batch_size 32 \
 --gpus="4," \
 --precision=16 \
 --progress_bar_refresh_rate 1 \
 --lr 3e-5 \
---distributed_backend=ddp \
 --val_check_interval 0.5 \
---accumulate_grad_batches 2 \
+--accumulate_grad_batches 1 \
 --default_root_dir "/ikerlariak/igarcia945/NerQA/bert-base-uncased" \
 --mrc_dropout 0.3 \
 --bert_dropout 0.1 \
@@ -18,7 +18,8 @@ python3 trainer.py \
 --weight_span 0.1 \
 --warmup_steps 0 \
 --gradient_clip_val 1.0 \
---flat
+--flat \
+--workers 8 \
 
 
 
